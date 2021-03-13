2e4326705621603a6db32532541de4f952e2a685
Manuel Hernández Méndez <maherme@localhost.localdomain>
2021-03-14 00:57:53 +0100

    Added Makefile to inotify Added error check for inotify_add_watch()

--- inotify/watch.c
+++ inotify/watch.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/inotify.h>
 #include <unistd.h>
 
@@ -9,7 +10,10 @@ int main(void){
     char buf[200];
 
     fd = inotify_init();
-    wd = inotify_add_watch(fd, "/home/maherme/Projects/examples/inotify", IN_CLOSE_WRITE);
+    if((wd = inotify_add_watch(fd, "/home/maherme/Projects/examples/inotify", IN_CLOSE_WRITE)) == -1){
+        printf("Error in inotify_add_watch() call\n");
+        exit(1);
+    }
 
     while(1){
         read(fd, buf, 200); /* Program execution will block here until an event happens */
